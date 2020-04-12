64a1ed2658b77d9113c4142c93604878f3a7b90c
Daniel Leal <37158127+daleal@users.noreply.github.com>
2020-04-12 04:01:20 -0400

    Revert "Feat/inventory item crud (#13)" (#14)
    This reverts commit 845772c3f9627e86d238ae0345b1d09767db412c.

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,10 @@ const KoaRouter = require('koa-router');
 
 const hello = require('./routes/hello');
 const index = require('./routes/index');
-const inventoryItems = require('./routes/inventory_items');
 
 const router = new KoaRouter();
 
 router.use('/', index.routes());
 router.use('/hello', hello.routes());
-router.use('/inventory-items', inventoryItems.routes());
+
 module.exports = router;
