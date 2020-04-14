7b59decc9f967291d9ace39b39ff5f12d82db316
José Tomás Escobar Docolomansky <37162196+JohnTrombon@users.noreply.github.com>
2020-04-13 23:45:09 -0400

    Basically copied from inventory-items crud (#26)

---
+++ src/migrations/20200413013318-create-project.js
@@ -0,0 +1,33 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      contact_info: {
+        type: Sequelize.TEXT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('projects');
+  }
+};
---
+++ src/models/project.js
@@ -0,0 +1,12 @@
+
+module.exports = (sequelize, DataTypes) => {
+  const project = sequelize.define('project', {
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    contactInfo: DataTypes.TEXT,
+  }, { underscored: true });
+  // projects.associate = function(models) {
+  //   // associations can be defined here
+  // };
+  return project;
+};
