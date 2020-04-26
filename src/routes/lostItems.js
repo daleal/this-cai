20a193cb0897e944b064055e464ae8b4917cea65
Daniel Leal <dlleal@uc.cl>
2020-04-26 00:19:35 -0400

    Save incorrect data when creating lost items and fix bug resulting of trying to render items from a snake_case folder that was renamed to camelCase

--- src/routes/lostItems.js
+++ src/routes/lostItems.js
@@ -2,59 +2,59 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter();
 
-router.get('lost_items.index', '/', async (ctx) => {
+router.get('lostItems.index', '/', async (ctx) => {
   const lostItems = await ctx.orm.lostItem.findAll();
-  await ctx.render('lost_items/index', {
+  await ctx.render('lostItems/index', {
     lostItems,
-    newPath: () => ctx.router.url('lost_items.new'),
-    showPath: (item) => ctx.router.url('lost_items.show', { id: item.id }),
-    editPath: (item) => ctx.router.url('lost_items.edit', { id: item.id }),
-    deletePath: (item) => ctx.router.url('lost_items.destroy', { id: item.id }),
+    newPath: () => ctx.router.url('lostItems.new'),
+    showPath: (item) => ctx.router.url('lostItems.show', { id: item.id }),
+    editPath: (item) => ctx.router.url('lostItems.edit', { id: item.id }),
+    deletePath: (item) => ctx.router.url('lostItems.destroy', { id: item.id }),
   });
 });
 
-router.get('lost_items.show', '/:id/show', async (ctx) => {
+router.get('lostItems.show', '/:id/show', async (ctx) => {
   const lostItem = await ctx.orm.lostItem.findByPk(ctx.params.id);
-  await ctx.render('lost_items/show', { lostItem });
+  await ctx.render('lostItems/show', { lostItem });
 });
 
-router.get('lost_items.new', '/new', async (ctx) => {
-  await ctx.render('lost_items/new');
+router.get('lostItems.new', '/new', async (ctx) => {
+  const lostItem = await ctx.orm.lostItem.build();
+  await ctx.render('lostItems/new', { lostItem });
 });
 
-router.post('lost_items.create', '/new', async (ctx) => {
+router.post('lostItems.create', '/new', async (ctx) => {
+  const lostItem = await ctx.orm.lostItem.build(ctx.request.body);
   try {
-    const { description } = ctx.request.body;
-    const lostItem = ctx.orm.lostItem.build({ description, taken: false });
     await lostItem.save({ fields: ['description', 'taken'] });
-    ctx.redirect(ctx.router.url('lost_items.index'));
+    ctx.redirect(ctx.router.url('lostItems.index'));
   } catch (validationError) {
     ctx.state.flashMessage.danger = validationError.message;
-    await ctx.render('lost_items/new');
+    await ctx.render('lostItems/new', { lostItem });
   }
 });
 
-router.get('lost_items.edit', '/:id/edit', async (ctx) => {
+router.get('lostItems.edit', '/:id/edit', async (ctx) => {
   const lostItem = await ctx.orm.lostItem.findByPk(ctx.params.id);
-  await ctx.render('lost_items/edit', { lostItem });
+  await ctx.render('lostItems/edit', { lostItem });
 });
 
-router.patch('lost_items.update', '/:id/edit', async (ctx) => {
+router.patch('lostItems.update', '/:id/edit', async (ctx) => {
   const lostItem = await ctx.orm.lostItem.findByPk(ctx.params.id);
   try {
     const { description } = ctx.request.body;
     await lostItem.update({ description });
-    ctx.redirect(ctx.router.url('lost_items.index'));
+    ctx.redirect(ctx.router.url('lostItems.index'));
   } catch (validationError) {
     ctx.state.flashMessage.danger = validationError.message;
-    await ctx.render('lost_items/edit', { lostItem });
+    await ctx.render('lostItems/edit', { lostItem });
   }
 });
 
-router.delete('lost_items.destroy', '/:id/destroy', async (ctx) => {
+router.delete('lostItems.destroy', '/:id/destroy', async (ctx) => {
   const lostItem = await ctx.orm.lostItem.findByPk(ctx.params.id);
   await lostItem.destroy();
-  ctx.redirect(ctx.router.url('lost_items.index'));
+  ctx.redirect(ctx.router.url('lostItems.index'));
 });
 
 module.exports = router;
