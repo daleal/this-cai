591d987b7ba64a91e2adf0c94add00425477c80b
felipearellanom <37161927+felipearellanom@users.noreply.github.com>
2020-05-09 19:58:02 -0400

    added passowords to seeds in order to ease testing and trouble shooting (#71)

--- src/seeds/20200419213513-demo-users.js
+++ src/seeds/20200419213513-demo-users.js
@@ -6,7 +6,8 @@ module.exports = {
     first_name: 'Felipe',
     last_name: 'Arellano',
     phone_number: '+5612348428',
-    role: 'admin',
+    role: 'administrator',
+    password: '$2b$10$Ncr7vaIceQF3ZMWB.a8wNOPs6gpZnTyJLlT8dc4jafjCLxwPopos6',
     created_at: new Date(),
     updated_at: new Date(),
   }, {
@@ -15,6 +16,7 @@ module.exports = {
     last_name: 'Brzovic',
     phone_number: '+5654994593',
     role: 'cai',
+    password: '$2b$10$Ncr7vaIceQF3ZMWB.a8wNOPs6gpZnTyJLlT8dc4jafjCLxwPopos6',
     created_at: new Date(),
     updated_at: new Date(),
   }, {
@@ -22,7 +24,8 @@ module.exports = {
     first_name: 'Juan',
     last_name: 'Schuwirth',
     phone_number: '+5615786458',
-    role: 'common',
+    role: 'user',
+    password: '$2b$10$Ncr7vaIceQF3ZMWB.a8wNOPs6gpZnTyJLlT8dc4jafjCLxwPopos6',
     created_at: new Date(),
     updated_at: new Date(),
   }, {
@@ -30,7 +33,8 @@ module.exports = {
     first_name: 'Daniel',
     last_name: 'Leal',
     phone_number: '+5685424597',
-    role: 'admin',
+    role: 'administrator',
+    password: '$2b$10$Ncr7vaIceQF3ZMWB.a8wNOPs6gpZnTyJLlT8dc4jafjCLxwPopos6',
     created_at: new Date(),
     updated_at: new Date(),
   }, {
@@ -38,7 +42,8 @@ module.exports = {
     first_name: 'Jose',
     last_name: 'Escobar',
     phone_number: '+5694568512',
-    role: 'admin',
+    role: 'administrator',
+    password: '$2b$10$Ncr7vaIceQF3ZMWB.a8wNOPs6gpZnTyJLlT8dc4jafjCLxwPopos6',
     created_at: new Date(),
     updated_at: new Date(),
   }], {}),
