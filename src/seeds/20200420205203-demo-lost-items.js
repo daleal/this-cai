939c0ad83f102462a210219b1f147104f789a9f4
felipearellanom <37161927+felipearellanom@users.noreply.github.com>
2020-07-01 20:17:56 -0400

    added location found to lost item seed (#188)
    * added location found to lost item seed
    
    * fix syntax error

--- src/seeds/20200420205203-demo-lost-items.js
+++ src/seeds/20200420205203-demo-lost-items.js
@@ -3,6 +3,7 @@
 module.exports = {
   up: (queryInterface) => queryInterface.bulkInsert('lost_items', [{
     description: 'Poleron azul marca Gap, talla M, estampado de 1969',
+    location_found: 'Sala A-3',
     taken: false,
     user_id: null,
     created_at: new Date(),
@@ -10,6 +11,7 @@ module.exports = {
     img: 'https://res.cloudinary.com/hrvt6ouf6/image/upload/v1591230854/seeds/hoodie_ewhm8o.jpg',
   }, {
     description: 'TUC de Jose Tomás Escobar',
+    location_found: 'Sala A-7 despues de una prueba',
     taken: true,
     user_id: 1,
     created_at: new Date(),
@@ -17,6 +19,7 @@ module.exports = {
     img: 'https://res.cloudinary.com/hrvt6ouf6/image/upload/v1591228842/seeds/tuc_dmkndb.jpg',
   }, {
     description: 'Cuaderno con apuntes Calculo 1, portada de Lamborghini',
+    location_found: 'Sala de estudio CAi',
     taken: false,
     user_id: null,
     created_at: new Date(),
@@ -24,13 +27,15 @@ module.exports = {
     img: 'https://res.cloudinary.com/hrvt6ouf6/image/upload/v1591228842/seeds/lambo_gzrxcs.jpg',
   }, {
     description: 'Guante gris con diseño de alpaca, derecho',
+    location_found: 'Patio de ingenieria',
     taken: false,
     user_id: null,
     created_at: new Date(),
     updated_at: new Date(),
     img: 'https://res.cloudinary.com/hrvt6ouf6/image/upload/v1591228842/seeds/guante_fblscu.jpg',
   }, {
-    description: 'Cargador computador Lenovo, 2V, encontrado en sala estudio K',
+    description: 'Cargador computador Lenovo, 2V',
+    location_found: 'Sala estudio K',
     taken: false,
     user_id: null,
     created_at: new Date(),
